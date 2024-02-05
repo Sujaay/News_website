30edf13f50f015caa3a083e2626beb1a7293217a
tanaya0222 <111778605+tanaya0222@users.noreply.github.com>
2024-02-05 15:21:38 +0530

    front page

--- news_website/src/App.js
+++ news_website/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+
+const articles = [
+  {
+    title: "Breaking News",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus fermentum metus, ut sollicitudin urna efficitur vel.",
+    image: "https://via.placeholder.com/150",
+    link: "#"
+  },
+  {
+    title: "Latest Technology",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus fermentum metus, ut sollicitudin urna efficitur vel.",
+    image: "https://via.placeholder.com/150",
+    link: "#"
+  },
+  {
+    title: "Sports Update",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus fermentum metus, ut sollicitudin urna efficitur vel.",
+    image: "https://via.placeholder.com/150",
+    link: "#"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>News Web App</h1>
       </header>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">World</a>
+        <a href="#">Technology</a>
+        <a href="#">Science</a>
+        <a href="#">Sports</a>
+      </nav>
+      <section className="Articles">
+        {articles.map((article, index) => (
+          <Article key={index} title={article.title} content={article.content} image={article.image} link={article.link} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+function Article({ title, content, image, link }) {
+  return (
+    <div className="Article">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <a href={link} className="ReadMore">Read More</a>
     </div>
   );
 }
