a776d11e9e1944975e466ce6e3622a6571d79d65
Sujaay <sujay.kumar47@gmail.com>
2024-04-07 23:19:18 +0530

    navbar update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
-// import Header from './components/Header';
-import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Category from './pages/Category'; // Make sure this import is correct
 import NotFound from './pages/NotFound'; // Make sure this import is correct
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        {/* <Header /> */}
         <div className="container">
-          <Sidebar/>
+          <Navbar/>
           <Routes> {/* Use Routes instead of Switch */}
             <Route path="/" element={<Home />} />
             <Route path="/category/:categoryName" element={<Category />} />
